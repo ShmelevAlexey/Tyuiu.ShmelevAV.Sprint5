66949029d4190da90627b31ca70c9a8ca63b669f
ShmelevAlexey <tyuiu.shmelevav@outlook.com>
2024-11-29 19:36:09 +0500

    Task5

---
+++ Tyuiu.ShmelevAV.Sprint5.Task5.V18.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ShmelevAV.Sprint5.Task5.V18.Lib
+{
+    public class DataService : ISprint5Task5V18
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 1;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string lineReplace = line.Replace('.', ',');
+                    string[] lineArray = lineReplace.Split(' ');
+
+                    foreach (string number in lineArray)
+                    {
+                        double lineParse = double.Parse(number);
+
+                        if (lineParse >= 10)
+                        {
+                            res *= lineParse;
+                        }
+                    }
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.ShmelevAV.Sprint5.Task5.V18/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.ShmelevAV.Sprint5.Task5.V18.Lib;
+
+namespace Tyuiu.ShmelevAV.Sprint5.Task5.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Шмелев А. В. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Шмелев Алексей Витальевич | ИИПб-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask5V18.txt (файл взять из      *");
+            Console.WriteLine("* архива согласно вашему варианту.                                        *");
+            Console.WriteLine("* Создать папку в ручную С:\\DataSprint5\\ и скопировать в неё файл) в    *");
+            Console.WriteLine("* котором есть набор значений. Найти произведение всех двузначных чисел в *");
+            Console.WriteLine("* файле. Полученный результат вывести на консоль. У вещественных значений *");
+            Console.WriteLine("* округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V18.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Произведение всех двузначных чисел в файле = " + res);
+            Console.ReadKey();
+        }
+    }
+}
