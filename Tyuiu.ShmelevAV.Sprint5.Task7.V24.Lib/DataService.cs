fb89fa41b14603688ebced174b00dd91a3a7cc63
ShmelevAlexey <tyuiu.shmelevav@outlook.com>
2024-11-30 00:33:52 +0500

    Task7

---
+++ Tyuiu.ShmelevAV.Sprint5.Task7.V24.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ShmelevAV.Sprint5.Task7.V24.Lib
+{
+    public class DataService : ISprint5Task7V24
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V24.txt");
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        strLine = Regex.Replace(line, @"\b\p{IsCyrillic}+\b", "слово");
+                    }
+
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+
+                    strLine = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.ShmelevAV.Sprint5.Task7.V24/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.ShmelevAV.Sprint5.Task7.V24.Lib;
+
+namespace Tyuiu.ShmelevAV.Sprint5.Task7.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Шмелев А. В. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Шмелев Алексей Витальевич | ИИПб-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask7V24.txt (файл взять из      *");
+            Console.WriteLine("* архива согласно вашему варианту. Создать папку в ручную                 *");
+            Console.WriteLine("* С:\\DataSprint5\\ и скопировать в неё файл) в котором есть набор        *");
+            Console.WriteLine("* символьных данных.Заменить все русские слова на слово \"слово\".        *");
+            Console.WriteLine("* Полученный результат сохранить в файл OutPutDataFileTask7V24.txt.       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+            string pathSaveFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V24.txt");
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Находится в файле: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
